0e3964306dbaaa74397da57a8d1c23576bf750aa
llamouth <150949923+llamouth@users.noreply.github.com>
2024-07-02 12:26:39 -0400

    Feat: Created front end template

---
+++ front-end/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.scss'
+import { Routes, Route } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default App
