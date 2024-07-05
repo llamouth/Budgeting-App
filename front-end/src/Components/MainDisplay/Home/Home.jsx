59ca00cf6fb1428e4ce9cac827010cedd64ae293
llamouth <150949923+llamouth@users.noreply.github.com>
2024-07-05 17:23:32 -0400

    feat: added a home page

--- front-end/src/Components/MainDisplay/Home/Home.jsx
+++ front-end/src/Components/MainDisplay/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import "../styles/Home.scss"
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+
+    const navigate = useNavigate()
+
     return (
         <div className='home-container'>
-            <h1>Welcome to BudgetYaSelf!</h1>
+            <h1 onClick={() => navigate("/transactions")}>Welcome to BudgetYaSelf!</h1>
             <p>An app where you have a better management on your expenses. To get started simply click home to view your current transactions or click new to add a new one.</p>
         </div>
     );
