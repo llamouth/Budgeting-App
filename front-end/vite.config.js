7ec0887315b12571d4e77175ff5e840c4c299c3c
llamouth <150949923+llamouth@users.noreply.github.com>
2024-07-06 20:25:01 -0400

    test: test again'

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -4,13 +4,18 @@ import { resolve } from 'path';
 import { copyFileSync } from 'fs';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'copy-redirects',
+      generateBundle() {
+        const src = resolve(__dirname, '_redirects');
+        const dest = resolve(__dirname, 'dist', '_redirects');
+        copyFileSync(src, dest);
+      },
+    },
+  ],
   build: {
     outDir: 'dist',
-  },
-  buildEnd() {
-    const src = resolve(__dirname, '_redirects');
-    const dest = resolve(__dirname, 'dist', '_redirects');
-    copyFileSync(src, dest);
   },
 });
